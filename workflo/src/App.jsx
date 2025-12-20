f57888c0fea76a2adc22397114bbd0d5c6eb9726
arghya-online <arghya0nline555@gmail.com>
2025-12-20 14:22:07 +0530

    refactor: Clean up imports and improve component structure in App.jsx

--- workflo/src/App.jsx
+++ workflo/src/App.jsx
@@ -1,25 +1,26 @@
-import React, { Children } from "react";
-import supabase from "./utils/supabase";
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MyJobs from "./pages/my-jobs";
-import SavedJobs from "./pages/saved-job";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
+
 import AppLayout from "./layouts/app-layout";
 import Landing from "./pages/landing";
 import Onboarding from "./pages/onboarding";
 import JobListing from "./pages/jobListing";
-import PostJob from "./pages/post-job";
 import JobPage from "./pages/job";
-import { ThemeProvider } from "./components/ui/theme-provider";
-import { ClerkProvider } from "@clerk/clerk-react";
+import SavedJobs from "./pages/saved-job";
+import MyJobs from "./pages/my-jobs";
+import PostJob from "./pages/post-job";
 import ProtectedRoutes from "./components/protected-routes";
-import { BarLoader } from "react-spinners";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
   if (!PUBLISHABLE_KEY) {
     throw new Error("Missing Clerk publishable key");
   }
+
   const router = createBrowserRouter([
     {
       element: <AppLayout />,
@@ -83,20 +84,21 @@ function App() {
       ],
     },
   ]);
-}
-return (
-  <>
-    <ClerkProvider
-      appearance={{ theme: "simple" }}
-      publishableKey={PUBLISHABLE_KEY}
-    >
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </ClerkProvider>
 
-    <Analytics />
-  </>
-);
+  return (
+    <>
+      <ClerkProvider
+        appearance={{ theme: "simple" }}
+        publishableKey={PUBLISHABLE_KEY}
+      >
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </ClerkProvider>
+
+      <Analytics />
+    </>
+  );
+}
 
 export default App;
